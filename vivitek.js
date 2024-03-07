a736808a69a5e9070ea990eaa5579829f2bf4fdd
Joseph Adams <josephdadams@gmail.com>
2024-03-07 08:47:44 -0500

    upgrade to companion v3

--- vivitek.js
+++ vivitek.js
@@ -1,155 +1,139 @@
-var tcp = require('../../tcp');
-var instance_skel = require('../../instance_skel');
-var debug;
-var log;
+const { InstanceBase, InstanceStatus, TCPHelper, Regex, runEntrypoint } = require('@companion-module/base')
 
-function instance(system, id, config) {
-	var self = this;
+const UpgradeScripts = require('./src/upgrades')
 
-	// super-constructor
-	instance_skel.apply(this, arguments);
+class vivitekInstance extends InstanceBase {
+	constructor(internal) {
+		super(internal)
 
-	self.actions(); // export actions
+		// Assign the methods from the listed files to this class
+		Object.assign(this, {
+		
+		})
+	}
 
-	return self;
+	async init(config) {
+		this.configUpdated(config);
+	}
+
+	async configUpdated(config) {
+		this.config = config
+
+		this.initActions();
+
+		this.init_tcp();
+		this.updateStatus(InstanceStatus.Connecting);
+	}
+
+	getConfigFields() {
+		return [
+			{
+				type: 'static-text',
+				id: 'info',
+				width: 12,
+				label: 'Information',
+				value: 'This module will control Vivitek Projectors.'
+			},
+			{
+				type: 'textinput',
+				id: 'host',
+				label: 'Target IP',
+				width: 6,
+				regex: Regex.IP
+			}
+		]
+	}
+
+	async destroy() {
+		//close out any connections
+
+		if (this.socket !== undefined) {
+			this.socket.destroy();
+			delete this.socket;
+		}
+
+		this.debug('destroy', this.id);
+	}
+
+	initActions() {
+		let actions = {};
+	
+		actions['powerOn'] = {
+			name: 'Power On Projector',
+			options: [],
+			callback: (action) => {
+				let cmd = 'op power.on';
+				this.send(cmd);
+			}
+		}
+	
+		actions['powerOff'] = {
+			name: 'Power Off Projector',
+			options: [],
+			callback: (action) => {
+				let cmd = 'op power.off';
+				this.send(cmd);
+			}
+		}
+	
+		actions['shutterOpen'] = {
+			name: 'Open Shutter',
+			options: [],
+			callback: (action) => {
+				let cmd = 'op shutter -\rop blank 0';
+				this.send(cmd);
+			}
+		}
+	
+		actions['shutterClose'] = {
+			name: 'Close Shutter',
+			options: [],
+			callback: (action) => {
+				let cmd = 'op shutter +\rop blank 1';
+				this.send(cmd);
+			}
+		}
+	
+		this.setActionDefinitions(actions);
+	}
+
+	init_tcp () {
+		let self = this;
+
+		if (this.socket !== undefined) {
+			this.socket.destroy();
+			delete this.socket;
+		}
+	
+		if (this.config.host) {
+			this.socket = new TCPHelper(this.config.host, 7000);
+	
+			this.socket.on('connect', function () {
+				self.updateStatus(InstanceStatus.Ok);
+				self.log('debug', "Connected");
+				self.send('op status ?');
+			});
+
+			this.socket.on('data', function (data) {
+				self.log('debug', "Received: " + data);
+			});
+
+			this.socket.on('error', function (err) {
+				self.updateStatus(InstanceStatus.Error, err);
+				self.log('error', "Network error: " + err);
+			});
+		}
+	}
+
+	send(cmd) {
+		if (this.socket !== undefined && this.socket.isConnected) {
+			this.socket.send(cmd+"\r");
+			this.log('debug',"Cmd Sent: " + cmd);
+		}
+		else {
+			this.log('debug', 'Socket not connected :(');
+		}
+	}
 }
 
-instance.prototype.updateConfig = function(config) {
-	var self = this;
-
-	self.config = config;
-
-	self.init_tcp();
-};
-
-instance.prototype.init = function() {
-	var self = this;
-
-	debug = self.debug;
-	log = self.log;
-
-	self.status(self.STATUS_UNKNOWN, 'Connecting');
-
-
-	self.init_tcp();
-	// Initial connect to check status
-	self.send('op status ?');
-};
-
-instance.prototype.init_tcp = function () {
-	var self = this;
-
-	// var connected = false;
-
-	if (self.socket !== undefined) {
-		self.socket.destroy();
-		delete self.socket;
-	}
-
-	if (self.config.host) {
-		self.socket = new tcp(self.config.host, 7000);
-
-		self.socket.on('status_change', function (status, message) {
-			self.status(status, message);
-		});
-
-		self.socket.on('error', function (err) {
-			self.log('debug', "Network error", err);
-			self.status(self.STATE_ERROR, err);
-			self.log('error', "Network error: " + err.message);
-		});
-
-		self.socket.on('connect', function () {
-			self.status(self.STATE_OK);
-			self.log('debug', "Connected");
-		});
-	}
-};
-
-instance.prototype.send = function(cmd) {
-	var self = this;
-
-	if (self.socket !== undefined && self.socket.connected) {
-		self.socket.send(cmd+"\r");
-		self.log('debug',"Cmd Sent: " + cmd);
-	}
-	else {
-		self.log('debug', 'Socket not connected :(');
-	}
-};
-
-
-// Return config fields for web config
-instance.prototype.config_fields = function () {
-	var self = this;
-	return [
-		{
-			type: 'textinput',
-			id: 'host',
-			label: 'Target IP',
-			width: 6,
-			regex: self.REGEX_IP
-		}
-	]
-};
-
-// When module gets deleted
-instance.prototype.destroy = function() {
-	var self = this;
-
-	if (self.socket !== undefined) {
-		self.socket.destroy();
-		delete self.socket;
-	}
-};
-
-
-instance.prototype.actions = function(system) {
-	var self = this;
-
-	self.setActions({
-		'powerOn':        { label: 'Power On Projector' },
-		'powerOff':       { label: 'Power Off Projector' },
-		'shutterOpen':    { label: 'Open Shutter' },
-		'shutterClose':   { label: 'Close Shutter' }
-
-	});
-};
-
-instance.prototype.action = function(action) {
-	var self = this;
-	var id = action.action;
-	var cmd
-
-	switch (id){
-
-		case 'powerOn':
-			cmd = 'op power.on';
-			break;
-
-		case 'powerOff':
-			cmd = 'op power.off';
-			break;
-
-		case 'shutterOpen':
-			//Some projectors require the shutter command and some use blank
-			cmd = 'op shutter -\rop blank 0';
-			break;
-
-		case 'shutterClose':
-			//Some projectors require the shutter command and some use blank
-			cmd = 'op shutter +\rop blank 1';
-			break;
-
-	}
-
-	if (cmd !== undefined) {
-		self.send(cmd);
-	}
-
-	// debug('action():', action);
-
-};
-
-instance_skel.extendedBy(instance);
-exports = module.exports = instance;
+runEntrypoint(vivitekInstance, UpgradeScripts);
